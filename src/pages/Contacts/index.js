2714cc84e71a24f85a4da827d8c6f9f21b6ce552
Volodymyr <citynight.immortal@gmail.com>
2021-05-24 13:35:31 +0300

    Components, styles, fetch hook

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,43 @@
+import { Container, Grid, LinearProgress, Typography } from "@material-ui/core";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { useContacts } from "../../hooks/useContacts";
+import { ContactsTable } from "../ContactsTable";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      marginTop: theme.spacing(4),
+    },
+    titleContainer: {
+      marginBottom: theme.spacing(4),
+    },
+  })
+);
+
 export const Contacts = () => {
-    return <div>Contacts</div>
-}
+  const classes = useStyles();
+  const contacts = useContacts();
+
+  return (
+    <Container className={classes.root}>
+      <Grid container>
+        <Grid className={classes.titleContainer}>
+          <Typography variant="h3" component="h3">
+            Contacts
+          </Typography>
+        </Grid>
+        {contacts.isError ? (
+          <div>...Error...!</div>
+        ) : (
+          <Grid item xs={12}>
+            {contacts.isLoading ? (
+              <LinearProgress/>
+            ) : (
+              <ContactsTable data={contacts.data} />
+            )}
+          </Grid>
+        )}
+      </Grid>
+    </Container>
+  );
+};
